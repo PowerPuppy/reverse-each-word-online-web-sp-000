b52af741d8944cc749442d7f90124c81d46728d5
Chris Cheong <chrisxcheong@gmail.com>
2019-07-11 22:30:06 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,5 +1,5 @@
 def reverse_each_word(string)
-new_string = string.split
-reversed = new_string.collect {|word| word.reverse}
-reversed.join(" ")
+  new_string = string.split
+  reversed = new_string.collect {|word| word.reverse}
+  reversed.join(" ")
 end
